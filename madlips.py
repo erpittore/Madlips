f58d6b0a12f610073ec592329e2926e431aec549
erpittore <73447714+erpittore@users.noreply.github.com>
2022-06-30 14:16:24 -0700

    Add files via upload

---
+++ madlips.py
@@ -0,0 +1,16 @@
+body_part = input ('Enter a body part:')
+plural_noun = input('Enter a plural noun:')
+celebrity = input ('Enter a celebrity:')
+animal =  input ('Enter an animal:')
+animal2 = input ('Enter an animal:')
+animal3 = input ('Enter an animal:')
+adjective = input ('Enter an adjective:')
+adjective2 = input ('Enter an adjective:')
+
+
+
+print('I was once impaled in the  '+ body_part +' by a sword')
+print(plural_noun + ' smell like toxic waste')
+print('I would love to kiss ' + celebrity)
+print('The ' + animal +' slapped the ' +animal2)
+print('The '+animal3  +adjective + ' ran into a ' + adjective2 + ' Darth Vader')
